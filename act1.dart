3f66c9b74ec840eb6391d01bf311614dc270226e
James <belacsijames1108@gmail.com>
2025-08-07 04:54:49 +0000

    Activity 1: Understanding the Dart

---
+++ act1.dart
@@ -0,0 +1,19 @@
+
+// Activity 1 
+
+// Number 1
+
+void main(){
+  String user = "Alice";
+  print("Welcome, " + user + "!" );
+}
+
+// Number 2
+
+void main(){
+  dynamic user = "Alice";
+  user = 25;
+  print("Welcome, $user!" );
+}
+
+// sa number 2, ang ginagawa ng dynamic ay para nyang ino-overwrite yung first value ng isang variable tapos yung pinaka latest na value yung ipiprint nya.
